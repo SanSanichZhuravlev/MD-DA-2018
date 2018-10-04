8c4aac5e73a37d07515c500e6e676744861109a4
SanSanichZhuravlev <SanSanichZhuravlev@users.noreply.github.com>
2018-10-04 10:34:57 +0500

    classwork1 creation

---
+++ R-classwork1.R
@@ -0,0 +1,12 @@
+11111111*11111111
+11111111*1111111
+length <- c(3, 7,12, 15, 20)
+width <- c(2, 5, 8, 11, 15)
+length + width
+plot(length, width)
+lines(length, width)
+subjects <- c("math"= 40, "english" = 40, "physical culture" = 10, "programming" = 150)
+barplot(subjects)
+drinks <-rnorm(5, mean = 450, sd = 4)
+drinks
+drinks > 455
