82bc5b9fe4960aa2edf983cedb573edda2815033
SanSanichZhuravlev <SanSanichZhuravlev@users.noreply.github.com>
2018-10-04 10:34:27 +0500

    introduction task creation

---
+++ R-Introduction.R
@@ -0,0 +1,30 @@
+1234+4567
+29-45 
+325/25
+56*12
+11*11
+111*111
+1111111*1111111
+options(digits=14)
+1111111*1111111
+options(width=40)
+5:32
+1:10
+(1:10)+3
+(1:10)^3
+3:4
+31 %% 7
+31 %/% 7
+x <-c(4, 1, 8, 9)
+y <-c(6, 2, 4, 3)
+plot(x, y)
+lines(x, y)
+x <- 1:10; y <- x^2;
+plot(x,y)
+learn <-c("stats" = 15, "math"= 10,"programming" = 30, "attempts" = 45)
+pie(learn)
+barplot(learn)
+Z <- rnorm(1000)
+hist(Z, prob = TRUE, main = "Гистограмма относительной частоты",
+sub = "Плотность распределения")
+curve(dnorm(x), from = -3, to = 3, add = TRUE, col = "blue")
